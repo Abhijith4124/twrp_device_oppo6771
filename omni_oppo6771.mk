a984ddc5e3f63431fe32fa2a1fee4522bf7524fe
Abhijith Dominic <abhijith4124@gmail.com>
2022-09-14 09:50:02 +0530

    Initial Files

---
+++ omni_oppo6771.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from oppo6771 device
+$(call inherit-product, device/alps/oppo6771/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := oppo6771
+PRODUCT_NAME := omni_oppo6771
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := oppo6771
+PRODUCT_MANUFACTURER := alps
